97e91078eb677653249e4972c728d2cee3ee7672
Joel Turkel <Jturkel@salsify.com>
2024-04-10 08:53:52 -0400

    Reproduce issue

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
-    config.filter_parameters += [:title]
+  
   end
 end
